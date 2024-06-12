1d01e58b994f1bafdbc05531d1873ee935433806
hrvieira <luizhrvieira@outlook.com>
2024-06-11 21:01:21 -0300

    feat: Tailwind instalado e configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-red-500 underline text-center">
+                Hello world!
+            </h1>
+        </>
+    );
 }
-
-export default App
+export default App;
