7301a50bb885845927da060ad4a601fd34244873
hrvieira <luizhrvieira@outlook.com>
2024-06-22 22:40:49 -0300

    feat: adicionando a página de projetos e melhorando a responsividade.

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -13,123 +13,139 @@ import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 interface Props {
-     window?: () => Window;
+    window?: () => Window;
 }
 
 interface NavItem {
-     to: string;
-     label: string;
+    to: string;
+    label: string;
 }
 
 const navItemsDesktop: NavItem[] = [
-     { to: "/projetos", label: "Projetos" },
-     { to: "/sobremim", label: "Sobre mim" },
-     { to: "/contato", label: "Contato" },
+    { to: "/projetos", label: "Projetos" },
+    { to: "/sobremim", label: "Sobre mim" },
+    { to: "/contato", label: "Contato" },
 ];
 
 const navItemsMobile: NavItem[] = [
-     { to: "/", label: "Início" },
-     { to: "/projetos", label: "Projetos" },
-     { to: "/sobremim", label: "Sobre mim" },
-     { to: "/contato", label: "Contato" },
+    { to: "/", label: "Início" },
+    { to: "/projetos", label: "Projetos" },
+    { to: "/sobremim", label: "Sobre mim" },
+    { to: "/contato", label: "Contato" },
 ];
 
 const NavBar: React.FC<Props> = (props: Props) => {
-     const { window } = props;
-     const [mobileOpen, setMobileOpen] = React.useState(false);
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-     const handleDrawerToggle = () => {
-          setMobileOpen((prevState) => !prevState);
-     };
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
 
-     const drawer = (
-         <Box
-             onClick={handleDrawerToggle}
-             sx={{ textAlign: "center", color: "#fff" }}
-         >
-             <List className="divide-y divide-gray-400 font-[montserratBold]">
-                 {navItemsMobile.map((item) => (
-                     <ListItem key={item.to} disablePadding>
-                         <ListItemButton sx={{ justifyContent: "center" }}>
-                             <Link to={item.to}>
-                                 <ListItemText primary={item.label} />
-                             </Link>
-                         </ListItemButton>
-                     </ListItem>
-                 ))}
-             </List>
-         </Box>
-     );
+    const drawer = (
+        <Box
+            onClick={handleDrawerToggle}
+            sx={{ textAlign: "center", color: "#fff" }}
+        >
+            <List className="divide-y divide-gray-400 font-[montserratBold]">
+                {navItemsMobile.map((item) => (
+                    <ListItem key={item.to} disablePadding>
+                        <ListItemButton sx={{ justifyContent: "center" }}>
+                            <Link to={item.to}>
+                                <ListItemText primary={item.label} />
+                            </Link>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
 
-     const container =
-          window !== undefined ? () => window().document.body : undefined;
+    const container =
+        window !== undefined ? () => window().document.body : undefined;
 
-     return (
-         <header className="font-[montserratBold]">
-             <Box sx={{ display: "flex" }}>
-                 <AppBar
-                     component="nav"
-                     sx={{
-                         display: "flex",
-                         background: "#000",
-                         position: "relative",
-                     }}
-                 >
-                     <Toolbar className="max-sm:w-[90%] w-[80%] flex justify-between mx-auto">
-                         <Link
-                             className="text-[1.5rem] hover:text-gray-300"
-                             id="inicio"
-                             to="/"
-                         >
-                             Início
-                         </Link>
-                         <IconButton
-                             color="inherit"
-                             aria-label="open drawer"
-                             edge="end"
-                             onClick={handleDrawerToggle}
-                             sx={{ display: { sm: "none" } }}
-                         >
-                             <MenuIcon />
-                         </IconButton>
-                         <Box sx={{ display: { xs: "none", sm: "block" } }}>
-                             {navItemsDesktop.map((item) => (
-                                 <Button key={item.to} sx={{ color: "#fff" }}>
-                                     <Link
-                                         className="block normal-case p-2 hover:text-gray-300 font-[montserratBold] text-[1.2rem]"
-                                         to={item.to}
-                                     >
-                                         {item.label}
-                                     </Link>
-                                 </Button>
-                             ))}
-                         </Box>
-                     </Toolbar>
-                 </AppBar>
-                 <nav>
-                     <Drawer
-                         container={container}
-                         variant="temporary"
-                         open={mobileOpen}
-                         onClose={handleDrawerToggle}
-                         ModalProps={{
-                             keepMounted: true,
-                         }}
-                         sx={{
-                             display: { xs: "block", sm: "none" },
-                             "& .MuiDrawer-paper": {
-                                 boxSizing: "border-box",
-                                 width: 300,
-                                 backgroundColor: "#565656",
-                             },
-                         }}
-                     >
-                         {drawer}
-                     </Drawer>
-                 </nav>
-             </Box>
-         </header>
-     );
+    return (
+        <header className="font-[montserratBold]">
+            <Box sx={{ display: "flex" }}>
+                <AppBar
+                    component="nav"
+                    sx={{
+                        display: "flex",
+                        background: "#000",
+                        position: "relative",
+                    }}
+                >
+                    <Toolbar className="flex justify-center">
+                        <div className="max-sm:w-[90%] w-[80%] flex items-center justify-between">
+                            <Link
+                                className="text-[1.5rem] hover:text-gray-300"
+                                id="inicio"
+                                to="/"
+                            >
+                                Início
+                            </Link>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                edge="end"
+                                onClick={handleDrawerToggle}
+                                sx={{ display: { md: "none" } }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Box
+                                className=""
+                                sx={{
+                                    display: {
+                                        xs: "none",
+                                        md: "block",
+                                    },
+                                }}
+                            >
+                                {navItemsDesktop.map((item) => (
+                                    <Button
+                                        key={item.to}
+                                        sx={{
+                                            color: "#fff",
+                                            margin: "0px 0px 0px 0px",
+                                        }}
+                                    >
+                                        <Link
+                                            className="block normal-case p-2 hover:text-gray-300 font-[montserratBold] text-[1.2rem]"
+                                            to={item.to}
+                                        >
+                                            {item.label}
+                                        </Link>
+                                    </Button>
+                                ))}
+                            </Box>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <nav>
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{
+                            keepMounted: true,
+                        }}
+                        sx={{
+                            display: { xs: "block", sm: "none" },
+                            "& .MuiDrawer-paper": {
+                                boxSizing: "border-box",
+                                width: 300,
+                                backgroundColor: "#565656",
+                            },
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </nav>
+            </Box>
+        </header>
+    );
 };
 
 export default NavBar;
--- src/components/projetos/Projetos.tsx
+++ src/components/projetos/Projetos.tsx
@@ -1,8 +1,81 @@
+import { Link } from "react-router-dom";
+
+interface Card {
+    imagem: string;
+    nome: string;
+    descricao: string;
+    deploy: string;
+}
+
+const cards: Card[] = [
+    {
+        imagem: "https://ik.imagekit.io/hrvieira/Portf%C3%B3lio/portfolio.png?updatedAt=1719098015142",
+        nome: "Portfólio",
+        descricao:
+            "Portfólio criado usando TypeScript, React e Tailwind. Site responsivo com o intuito de facilitar a divulgação de meu trabalho.",
+        deploy: "https://portfolio-luizhrvieira.vercel.app/",
+    },
+    {
+        imagem: "https://ik.imagekit.io/hrvieira/Portf%C3%B3lio/greenconomy.png?updatedAt=1719106029375",
+        nome: "Green Economy",
+        descricao:
+            "Projeto usando TypeScript, Nest, Jest, React e Tailwind. E-commerce baseado na ODS-12, onde pessoas podem comprar e vender produtos sustentáveis, ajudando na diminuição de materiais jogados no meio ambiente, consumo e produção sustentável.",
+        deploy: "https://greenconomy.netlify.app/",
+    },
+    {
+        imagem: "https://ik.imagekit.io/hrvieira/Portf%C3%B3lio/universe.png?updatedAt=1719097994710",
+        nome: "Universe",
+        descricao:
+            "Projeto criado usando TypeScript, React e CSS3. Site para insetivar o uso de um portfólio e integração a API do GitHub",
+        deploy: "https://universe-react.vercel.app/",
+    },
+    {
+        imagem: "https://ik.imagekit.io/hrvieira/Portf%C3%B3lio/Beige%20Brown%20Aesthetic%20Save%20The%20Date%20Editable%20Mockup%20Instagram%20Post.png?updatedAt=1719100489486",
+        nome: "Blog Pessoal",
+        descricao:
+            "Projeto criado usando TypeScript, Nest, Jest, React e Tailwind. Criar um blog com Front end e Back end bem estruturado. Podendo fazer postagens com diversos temas.",
+        deploy: "https://blog-pessoal-hrvieira.vercel.app/",
+    },
+    {
+        imagem: "https://ik.imagekit.io/hrvieira/Portf%C3%B3lio/linksgen.png?updatedAt=1719106078559",
+        nome: "Links Generation",
+        descricao:
+            "Projeto criado com JavaScript, HTML5 e CSS3. Site simples para facilidade do curso da Generation Brasil, onde a turma JS 05 conseguia acessar com mais facilidade os links que precisavamos para o nosso dia a dia de estudos.",
+        deploy: "https://linksgen.vercel.app/",
+    },
+];
+
 function Projetos() {
     return (
-        <main className="min-h-[65dvh]">
-            <div className="w-full bg-red-600 bg-opacity-80 font-bold text-[3rem] py-4 flex justify-around">
-                <h1>Projetos - À implementar!</h1>
+        <main className="my-4 w-full">
+            <div className="grid max-mobile:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 lg:grid-cols-4 gap-4 w-[90%] mx-auto">
+                {cards.map((card) => (
+                    <div className="flex flex-col mt-6 shadow-md hover:shadow-2xl transition duration-500 rounded-2xl place-items-center border-2 ">
+                        <img
+                            className="rounded-t-xl shadow-lg box-border"
+                            src={card.imagem}
+                            alt="card-image"
+                        />
+                        <div className="flex flex-col flex-grow w-[85%]">
+                            <div className="py-2 mt-2 grow">
+                                <h1 className="mb-1 text-[1.2rem] font-[montserratBold] text-center">
+                                    {card.nome}
+                                </h1>
+                                <h3 className="font-[glacialIndifference] text-justify">
+                                    {card.descricao}
+                                </h3>
+                            </div>
+
+                            <Link
+                                to={card.deploy}
+                                className="text-gray-700 hover:text-black hover:underline hover:underline-offset-2 font-bold rounded-lg shadow-lg py-2 mb-2 border-2 text-center"
+                                target="_blank"
+                            >
+                                Acesse aqui
+                            </Link>
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     );
